394338c9079772acfbf3f179105a87f4310173fd
Youtubbi <112695689+Drakkadakka@users.noreply.github.com>
2024-11-23 14:41:05 +1030

    fixed integration
    added integration into AI system

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='voice-changer',
+    name='voice_changer',
     version='1.5.3.18a',
     packages=find_packages(),
     install_requires=[
@@ -13,4 +13,9 @@ setup(
     description='A voice changer library',
     author='w-okada',
     url='https://github.com/w-okada/voice-changer',
+    entry_points={
+        'console_scripts': [
+            'voice_changer=voice_changer:main',  # Adjust 'main' to the actual function you want to run
+        ],
+    },
 )
